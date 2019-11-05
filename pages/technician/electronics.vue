d6211cc731cdf502b343fd49a70ef1d7c773149c
Jawad <jawainc@gmail.com>
2019-11-05 16:41:14 +0500

    check

---
+++ pages/technician/electronics.vue
@@ -0,0 +1,168 @@
+<template>
+  <div :class="{'hide-left-bar':!leftMenu}" class="flex main-panel">
+    <!--  Left Side Bar  -->
+    <LeftMenu/>
+
+    <!-- Main Contents -->
+    <div class="flex-grow mb-20">
+      <div class="main-contents">
+
+        <!--Header Bar-->
+        <div class="header-bar lg:flex items-center mx-auto">
+           <nuxt-link class="header-menu" to="/technician/electronics/add-part">
+             Add Part
+           </nuxt-link>
+
+          <nuxt-link class="header-menu" to="/technician/electronics/add-component">
+            Add Component
+          </nuxt-link>
+
+          <nuxt-link class="header-menu" to="/technician/electronics/add-pcb">
+            Add PCB
+          </nuxt-link>
+
+
+        </div>
+        <nuxt-child></nuxt-child>
+
+        <!--Bot Buttons-->
+        <div class="mt-10 w-full mx-auto button-bar flex">
+          <div class="w-1/2 text-left">
+            <button v-if="step > 0" @click="back" class="btn pill-button px-16 py-1">Back</button>
+          </div>
+          <div class="w-1/2 text-right">
+            <button @click="next" class="btn pill-button pill-button--ideeza px-16 py-1">Next</button>
+          </div>
+        </div>
+
+      </div>
+
+
+    </div>
+
+    <!--Right Sliders-->
+    <div :class="{'active': activeDrawer === 'cart'}" class="right-drawer right-drawer--cart  fixed-center-v right-0 bg-transparent flex handle-margin z-50">
+      <!--handle-->
+      <div>
+        <div @click="handle('cart')" class="handle">
+          <font-awesome-icon class="ml-1 h-6 text-gray-600 text-3xl" :icon="['fas', 'shopping-cart']"/>
+        </div>
+      </div>
+      <div class="handle-contents handle-contents--cart"></div>
+    </div>
+
+    <div :class="{'active': activeDrawer === 'link'}" class="right-drawer fixed-center-v right-0 bg-transparent flex z-40">
+      <!--handle-->
+      <div>
+        <div @click="handle('link')" class="handle">
+          <font-awesome-icon class="ml-1 h-6 text-gray-600 text-3xl" :icon="['fas', 'link']"/>
+        </div>
+      </div>
+      <div class="handle-contents"></div>
+    </div>
+
+    <div :class="{'active': activeDrawer === 'options'}" class="right-drawer fixed-center-v right-0 bg-transparent flex handle-margin-2 z-30">
+      <!--handle-->
+      <div>
+        <div @click="handle('options')" class="handle z-50">
+          <font-awesome-icon class="ml-1 h-6 text-gray-600 text-3xl" :icon="['fas', 'cog']"/>
+        </div>
+      </div>
+      <div class="handle-contents"></div>
+    </div>
+
+  </div>
+
+
+</template>
+
+<script>
+  import LeftMenu from '~/components/technician/common-left-side-menu.vue'
+
+  export default {
+    layout: 'technician',
+    name: "electronics-index",
+    components: {
+      LeftMenu
+    },
+    data: function () {
+      return {
+        activeDrawer: null,
+        step: 0
+      }
+    },
+    computed: {
+      leftMenu() {
+        return this.$store.state.usermenu.openLeftMenu;
+      }
+    },
+    mounted() {
+
+    },
+    methods: {
+      back() {
+
+      },
+      next() {
+
+      },
+      handle(drawer){
+        if(this.activeDrawer === drawer)
+          this.activeDrawer = null;
+        else
+          this.activeDrawer = drawer;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .handle{
+    @apply bg-white shadow-md px-6 py-4 rounded-l-lg cursor-pointer;
+    height: fit-content;
+    margin-left: -86px;
+  }
+  .handle-margin{
+    margin-top: -50px;
+  }
+  .handle-margin-2{
+    margin-top: 50px;
+  }
+  .right-drawer{
+    @apply bg-white;
+    width: 450px;
+    margin-right: -450px;
+  }
+  .right-drawer--cart{
+    width: 650px;
+    margin-right: -650px;
+  }
+  .right-drawer.active{
+    margin-right: 0px;
+  }
+
+  .handle-contents{
+    @apply bg-white p-4 shadow rounded-bl-lg;
+    height: 300px;
+    transition: all 0.3s ease-in;
+  }
+  .handle-contents--cart{
+
+  }
+  .header-bar{
+    width: fit-content;
+  }
+  .button-bar{
+    width: 100%;
+    max-width: 950px;
+  }
+  .header-menu{
+    @apply px-6 py-3 bg-white text-lg text-gray-600 cursor-pointer mr-6 font-semibold;
+  }
+  .header-menu:hover{
+    @apply text-ideeza;
+  }
+  .header-menu.active-link{
+    @apply bg-transparent text-ideeza border border-solid border-gray-300;
+  }
+</style>
---
+++ pages/technician/electronics/add-component/index.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="email-support-container mx-auto mt-32">
+      <div class="hidden lg:block stepper-bar bg-gray-200 relative mx-auto mb-20">
+        <div class="stepper-bar-overlay bg-ideeza-dark" :style="`width: ${overlayWidth}%` "></div>
+        <div class="absolute top-0 left-0 h-full w-full z-50 flex justify-between">
+
+          <!--1-->
+          <div class="stepper-icon-wrapper relative" :class="{'active': step >= 0}">
+            <span class="absolute-center-h-v" >1</span>
+            <div class="stepper-text stepper-text--ask" :class="{'active': step >=0 }">Schematic</div>
+          </div>
+          <!--2-->
+          <div class="stepper-icon-wrapper relative" :class="{'active': step >= 1}">
+            <span  class="absolute-center-h-v" >2</span>
+            <div class="stepper-text stepper-text--review" :class="{'active': step >= 1}">Design</div>
+          </div>
+          <!--3-->
+          <div class="stepper-icon-wrapper relative" :class="{'active': step >= 2 }">
+            <span  class="absolute-center-h-v" >3</span>
+            <div class="stepper-text stepper-text--complete" :class="{'active': step >= 2 }">Code</div>
+          </div>
+          <!--4-->
+          <div class="stepper-icon-wrapper relative" :class="{'active': step >= 3 }">
+            <span  class="absolute-center-h-v" >4</span>
+            <div class="stepper-text stepper-text--complete" :class="{'active': step >= 3 }">General</div>
+          </div>
+
+
+        </div>
+      </div>
+
+      <div class="search-container flex w-fit-content bg-white items-center content-center border border-solid border-gray-300">
+        <div class="h-12 relative w-10">
+          <font-awesome-icon class="ml-1 h-4 text-gray-400 absolute-center-h-v" :icon="['fas', 'search']"/>
+        </div>
+        <input placeholder="Add more components" class="bg-white flex-grow outline-none h-8 text-gray-800 pr-3">
+      </div>
+
+      <div class="w-full bg-gray-400 mt-10 components-container"></div>
+
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+      data: function () {
+        return {
+          step: 0
+        }
+      },
+
+      computed: {
+        overlayWidth () {
+          if(this.step === 0)
+            return 0;
+          else if(this.step === 1)
+            return 50;
+          else if(this.step === 2)
+            return 99;
+          else if(this.step === 3)
+            return 99;
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .components-container{
+    height: 400px;
+  }
+  .email-support-container{
+    width: 100%;
+    max-width: 950px;
+  }
+  .stepper-bar{
+    width: 600px;
+    height: 15px;
+  }
+  .stepper-bar-overlay{
+    @apply rounded-r-lg;
+    transition: all 0.3s ease-in;
+    height: 100%;
+    width: 97%;
+    margin-left: 2px;
+  }
+  .stepper-icon-wrapper{
+    @apply rounded-full bg-gray-200 font-semibold;
+    width: 50px;
+    height: 50px;
+    margin-top: -20px;
+    transition: all 0.3s ease-in;
+  }
+
+  .stepper-icon-wrapper.active{
+    @apply bg-ideeza-dark;
+  }
+  .stepper-icon-wrapper.active {
+    @apply text-white;
+  }
+  .stepper-text{
+    @apply absolute font-semibold text-gray-600;
+    margin-top: 60px;
+  }
+  .stepper-text--ask{
+    margin-left: 10px;
+  }
+  .stepper-text--review{
+    margin-left: -2px;
+  }
+  .stepper-text--complete{
+    margin-left: -12px;
+  }
+
+  @screen lg{
+    .thankyou-email{
+      height: 500px;
+    }
+  }
+</style>
