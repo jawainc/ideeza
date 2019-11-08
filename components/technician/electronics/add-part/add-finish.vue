5aa2fbfedd541a80dec7660aa877ce8ddc76c692
Jawad <jawainc@gmail.com>
2019-11-09 01:38:32 +0500

    final electronics

--- components/technician/electronics/add-part/add-finish.vue
+++ components/technician/electronics/add-part/add-finish.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="bg-white shadow-md w-full text-center flex justify-between items-center font-semibold text-5xl py-32">
-
+  <div class="bg-white shadow-md w-full text-center flex flex-col justify-between items-center font-semibold text-5xl py-32">
+    <div class="mx-auto">
+      <img src="~/static/images/e-processor.png" alt="">
+    </div>
     <div class="mx-auto">Congratulations!<br>
     Your part is added</div>
   </div>
